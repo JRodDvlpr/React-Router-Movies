468750f01dc074946a96751599d5fdf41c8fd276
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-04 17:02:15 -0500

    :tada: Movie/:id route working

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// import MovieCard from './MovieCard';
+import { Link } from 'react-router-dom';
 
 const Movie = (props) => {
   const [movie, setMovie] = useState();
  
   useEffect(() => {
-    const id = 1;
-    // change ^^^ that line and grab the id from the URL
-    // You will NEED to add a dependency array to this effect hook
-
-       axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+    const id = props.match.params.id;
+    
+       axios.get(`http://localhost:5000/api/movies/${id}`)
         .then(response => {
           setMovie(response.data);
         })
@@ -32,6 +31,7 @@ const Movie = (props) => {
 
   const { title, director, metascore, stars } = movie;
   return (
+    <Link to={`movies/${props.id}`}>
     <div className="save-wrapper">
       <div className="movie-card">
         <h2>{title}</h2>
@@ -51,6 +51,7 @@ const Movie = (props) => {
       </div>
       <div className="save-button">Save</div>
     </div>
+    </Link>
   );
 }
 
