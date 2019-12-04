db11ac1a563dc45d6ec8507dbdff57e7b6a1b52a
JRodDvlpr <Mialienproductions@gmail.com>
2019-12-04 13:52:27 -0500

    :bug: Fixed

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Router} from 'react-router-dom';
-
+import { BrowserRouter as Router} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-<Route exact path="/" component={MovieList} />
+
 
 const rootElement = document.getElementById('root');
 
